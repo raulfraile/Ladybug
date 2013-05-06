dc5193a2376cad9fd1d54af78cae3033c1efda8d
Volker Dusch <github@wallbash.com>
2013-05-06 12:47:37 +0300

    Issue non-success exit code on ladybug_dump_die()

--- lib/Ladybug/helpers.php
+++ lib/Ladybug/helpers.php
@@ -30,7 +30,7 @@ function ladybug_dump_die(/*$var1 [, $var2...$varN]*/)
 {
     $ladybug = \Ladybug\Dumper::getInstance();
     echo call_user_func_array(array($ladybug,'dump'), func_get_args());
-    die();
+    die(1);
 }
 
 function ladybug_dump_return(/*$format $var1 [, $var2...$varN]*/)
